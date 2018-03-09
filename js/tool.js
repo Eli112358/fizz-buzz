6bd8037803e526892b95d4ce585996c122b65902
Eli112358 <Eli112358@users.noreply.github.com>
2018-03-08 22:35:44 -0500

    Finnish tool code

--- js/tool.js
+++ js/tool.js
@@ -3,12 +3,18 @@ function initTool() {
   tool = initModule('tool-', ['main', 'input', 'output']);
   tool.out = (str) => {tool.ele.output.innerHTML += str};
   tool.go = () => {
+    tool.reset(false);
     for(var i=0; i<getSettings().store.ele.range.value; i++) {
       tool.out(getFizzBuzz().step(tool.ele.input.value+i))
     }
   };
-  tool.reset = (full) => {};
-  tool.delta = (sign) => {};
+  tool.reset = (full) => {
+    if(full) tool.ele.input.value = '';
+    tool.ele.output.innerHTML = '';
+  };
+  tool.delta = (sign) => {
+    tool.ele.input.value = eval(tool.ele.input.value + sign + getSettings().store.ele.range.value);
+  };
 }
 function getTool() {
   return tool;
