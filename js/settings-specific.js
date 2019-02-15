fcb9d0f7fa265b6071c8ec2ae9032af602c59f02
Eli112358 <Eli112358@users.noreply.github.com>
2019-02-15 16:31:59 -0500

    Spaces to tabs

--- js/settings-specific.js
+++ js/settings-specific.js
@@ -1,35 +1,35 @@
 var settings = {};
 function initSettingsSpecific() {
-  initSettings({
-    'get': () => settings,
-    'set': (newValue) => {
-      settings = newValue
-    },
-    'store': ['range'],
-    'toggle': ['mode'],
-    'func': () => {
-      settings.toggle.setup({
-        'ele': settings.toggle.ele.mode,
-        'property': 'value',
-        'stored': true,
-        'values': ['Game', 'Tool'],
-        'func': () => {
-          ['mode-main', 'settings-section'].forEach((n) => {
-            [].forEach.call(getByClass(n), toggle)
-          })
-        }
-      });
-      initModifiableList({
-        'get': () => settings.words,
-        'set': (newValue) => {
-          settings.words = newValue
-        },
-        'prefix': 'words-'
-      });
-      loadMore();
-    }
-  })
+	initSettings({
+		'get': () => settings,
+		'set': (newValue) => {
+			settings = newValue
+		},
+		'store': ['range'],
+		'toggle': ['mode'],
+		'func': () => {
+			settings.toggle.setup({
+				'ele': settings.toggle.ele.mode,
+				'property': 'value',
+				'stored': true,
+				'values': ['Game', 'Tool'],
+				'func': () => {
+					['mode-main', 'settings-section'].forEach((n) => {
+						[].forEach.call(getByClass(n), toggle)
+					})
+				}
+			});
+			initModifiableList({
+				'get': () => settings.words,
+				'set': (newValue) => {
+					settings.words = newValue
+				},
+				'prefix': 'words-'
+			});
+			loadMore();
+		}
+	})
 }
 function getSettings() {
-  return settings;
+	return settings;
 }
--- js/tool.js
+++ js/tool.js
@@ -1,21 +1,21 @@
 var tool;
 function initTool() {
-  tool = initModule('tool-', ['main', 'input', 'output']);
-  tool.out = (str) => {tool.ele.output.innerHTML += str + '<br/>'};
-  tool.go = () => {
-    tool.reset(false);
-    for(var i=1; i<getSettings().store.ele.range.value; i++) {
-      tool.out(getFizzBuzz().step(tool.ele.input.value+i))
-    }
-  };
-  tool.reset = (full) => {
-    if(full) tool.ele.input.value = '';
-    tool.ele.output.innerHTML = '';
-  };
-  tool.delta = (sign) => {
-    tool.ele.input.value = eval(tool.ele.input.value + sign + getSettings().store.ele.range.value);
-  };
+	tool = initModule('tool-', ['main', 'input', 'output']);
+	tool.out = (str) => {tool.ele.output.innerHTML += str + '<br/>'};
+	tool.go = () => {
+		tool.reset(false);
+		for(var i=1; i<getSettings().store.ele.range.value; i++) {
+			tool.out(getFizzBuzz().step(tool.ele.input.value+i))
+		}
+	};
+	tool.reset = (full) => {
+		if(full) tool.ele.input.value = '';
+		tool.ele.output.innerHTML = '';
+	};
+	tool.delta = (sign) => {
+		tool.ele.input.value = eval(tool.ele.input.value + sign + getSettings().store.ele.range.value);
+	};
 }
 function getTool() {
-  return tool;
+	return tool;
 }
