e06ef595fec7286989d668e1e9f97a4399270faf
Eli112358 <Eli112358D@gmail.com>
2020-06-01 12:09:41 -0400

    Remove dead code

--- js/settings-body.js
+++ js/settings-body.js
@@ -1,7 +1,6 @@
 import { fixButtons } from '/javascripts/modules/header.js';
 import { ModifiableList } from '/javascripts/modules/modifiable-list.js';
 import { Settings } from '/javascripts/modules/settings.js';
-import { appendHtml } from '/javascripts/modules/snippet.js';
 import { body } from '../snippets/settings-body.js';
 
 const store = [
